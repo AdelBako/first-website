053a718c07c90453364182fc14835b338ace9b99
Adel Bako <adelabako96@gmail.com>
2024-04-20 23:22:09 -0400

    commit
    4/20/24

---
+++ scripts/myScript.js
@@ -0,0 +1,15 @@
+
+/******ALERT BOX ADDED FOR INDEX PAGE ONLY******/
+function myFunction() {
+    alert("Welcome pets of all ages, sizes and skills");
+  }
+
+
+
+
+
+/********DATE ADDED FOR ALL HTML PAGES*******/
+function date(){
+    document.getElementById ("date").innerHTML = year ;
+
+}
